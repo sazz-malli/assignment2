48274f7eb4ff488cbb12259be7bb6fda49e90148
Mallikarjun Sajjapuri <sazz.malli@gmail.com>
2018-08-14 15:52:45 -0700

    Assignment2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  getCurrentStatus = true;
+  inputText = null;
+
+  onClearData() {
+    this.inputText = '';
+    // if(this.inputText === ''){
+    //   console.log(this.inputText);
+    //   this.inputText = 'checking IF Condition';
+    //   console.log(this.inputText);
+    // }else {
+    //   this.inputText = 'else';
+    }
+
+  }
 }
